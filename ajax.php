e5f9126777b6ba68f130c46762d9a527f8d3191f
Владислав <gaad.ru@yandex.ru>
2019-09-26 15:12:32 +0500

    Последние шаги
    Все работает, осталось доделать авторизации и подгрузку контента

--- ajax.php
+++ ajax.php
@@ -5,21 +5,25 @@
 	$bdName = 'scicker';
 $link = mysqli_connect($host, $login, $password, $bdName);
 
-
 if (!empty($_REQUEST['object'])) {
 	$object = $_REQUEST['object'];
+	$changes = $_REQUEST['changes'];
 	preg_match_all('#id="(\d*?)"#su', $object, $match);
-
 	$id = $match[1][0];
-	$query = "SELECT * FROM `scickers` WHERE `id` ='$id'";
+
+	$query = "SELECT * FROM `scickers` WHERE `id_sticker` ='$id'";
 	$result = mysqli_query($link, $query);
 	for ($data = []; $row = mysqli_fetch_assoc($result); $data[] = $row);
+
 	if (empty($data)) {
-		$query = "INSERT INTO `scickers`(`id_sticker`, `object`) VALUES ('$id', '$object')";
+		$query = "INSERT INTO `scickers`(`id_sticker`, `object`, `changes`) VALUES ('$id', '$object', 'changes')";
 	} else {
-		$query = "";$query = "UPDATE `scickers` SET `object`= '$object' WHERE `id_sticker` = '$id'";
+		$query = "UPDATE `scickers` SET `object`= '$object', `changes` = '$changes' WHERE `id_sticker` = '$id'";
 	}
 
+} else {
+	$id = $_REQUEST['deleteObject'];
+	$query = "DELETE FROM `scickers` WHERE `id_sticker` = '$id'";
 }
 
 $result = mysqli_query($link, $query);
